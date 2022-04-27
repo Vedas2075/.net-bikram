a439363690eef02b4ecb729650e4fb524177fb6b
Bikram <zeedan.subba33@gmail.com>
2022-04-27 12:05:52 +0545

    first commit

--- Classses/CSharpFundamentals/EntryPoint.cs
+++ Classses/CSharpFundamentals/EntryPoint.cs
@@ -10,6 +10,6 @@ public class Program
         var d2 = calculator1.Subtract();
         var m2 = calculator1.Multiply();
         var di2 = calculator1.Divide();
-        Console.WriteLine("This is an outpput");
+         for(long) i= 10; 1<20
     }
 }
